7afbd93900f37aa524d0c0066e137a9a0b1a3e30
shafakyildiz <shafakyildiz@gmail.com>
2023-12-12 16:18:29 +0300

    Add popular component

---
+++ src/components/popular/Popular.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Popular.css";
+import product_data from "../assets/product_data.json";
+
+const Popular = () => {
+  return <div className="popular"></div>;
+};
+
+export default Popular;
