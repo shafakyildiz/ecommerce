ce2d79d12d832d0db4b184ae216147d5006c4286
shafakyildiz <shafakyildiz@gmail.com>
2023-12-12 16:49:39 +0300

    Add product data and render to the shop component

--- src/components/popular/Popular.jsx
+++ src/components/popular/Popular.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 import "./Popular.css";
-import product_data from "../assets/product_data.json";
+import { product_data } from "../assets/product_data.js";
+import Item from "../Item/Item";
 
 const Popular = () => {
-  return <div className="popular"></div>;
+  console.log(product_data);
+  return (
+    <div className="popular">
+      <h1>POPULAR IN GENERAL CATEGORY</h1>
+      <hr />
+      <div className="popular-item">
+        {product_data.map((item, index) => {
+          return (
+            <Item
+              key={index}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              old_price={item.old_price}
+              new_price={item.new_price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Popular;
