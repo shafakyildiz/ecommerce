2efa3ba51e71a0fff020092aef65586870e2c1bd
shafakyildiz <shafakyildiz@gmail.com>
2023-12-12 23:35:52 +0300

    Add new collections and offers components

---
+++ src/components/newcollections/NewCollections.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./NewCollections.css";
+import Item from "../Item/Item";
+import { new_collections } from "../assets/new_collections.js";
+
+const NewCollections = () => {
+  return (
+    <div className="new-collections">
+      <h1>NEW COLLECTIONS</h1>
+      <hr />
+      <div className="collections">
+        {new_collections.map((item, index) => {
+          return (
+            <Item
+              key={index}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              old_price={item.old_price}
+              new_price={item.new_price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default NewCollections;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Hero from "../components/hero/Hero";
+import Offers from "../components/offers/Offers";
 import Popular from "../components/popular/Popular";
+import NewCollections from "../components/newcollections/NewCollections";
 
 const Shop = () => {
   return (
-    <div>
+    <>
       <Hero />
       <Popular />
-    </div>
+      <Offers />
+      <NewCollections />
+    </>
   );
 };
 
