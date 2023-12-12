79f8ff17cf02b0bbf5264f126cf43999e5ae5705
shafakyildiz <shafakyildiz@gmail.com>
2023-12-12 14:51:02 +0300

    Add react-router-dom and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ShopCategory from "./pages/ShopCategory";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />}></Route>
+          <Route path="/men" element={<ShopCategory />}></Route>
+          <Route path="/women" element={<ShopCategory />}></Route>
+          <Route path="/kid" element={<ShopCategory />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
