d1cada7de3d43eadf78381d7b4b484d0aae24f03
shafakyildiz <shafakyildiz@gmail.com>
2023-12-12 23:35:34 +0300

    Add some assets

---
+++ src/components/assets/new_collections.js
@@ -0,0 +1,82 @@
+export let new_collections = [
+  {
+    id: "101",
+    name: "Laptop",
+    old_price: 1200.0,
+    new_price: 999.99,
+    image:
+      "https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: "102",
+    name: "Smartphone",
+    old_price: 800.0,
+    new_price: 699.99,
+    image:
+      "https://images.pexels.com/photos/5234774/pexels-photo-5234774.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: "103",
+    name: "Headphones",
+    old_price: 150.0,
+    new_price: 129.99,
+    image:
+      "https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: "104",
+    name: "Coffee Maker",
+    old_price: 60.0,
+    new_price: 49.99,
+    image:
+      "https://images.pexels.com/photos/324028/pexels-photo-324028.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: "105",
+    name: "Running Shoes",
+    old_price: 80.0,
+    new_price: 69.99,
+    image:
+      "https://images.pexels.com/photos/7432/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: "106",
+    name: "Digital Camera",
+    old_price: 450.0,
+    new_price: 399.99,
+    image:
+      "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    id: "107",
+    name: "Wireless Speaker",
+    old_price: 100.0,
+    new_price: 79.99,
+    image:
+      "https://images.pexels.com/photos/2651794/pexels-photo-2651794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: "108",
+    name: "Backpack",
+    old_price: 50.0,
+    new_price: 39.99,
+    image:
+      "https://images.pexels.com/photos/842947/pexels-photo-842947.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: "109",
+    name: "Fitness Tracker",
+    old_price: 75.0,
+    new_price: 59.99,
+    image:
+      "https://images.pexels.com/photos/437038/pexels-photo-437038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: "110",
+    name: "Smart Watch",
+    old_price: 200.0,
+    new_price: 179.99,
+    image:
+      "https://images.pexels.com/photos/267394/pexels-photo-267394.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+];
