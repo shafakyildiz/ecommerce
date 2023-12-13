d69b34515b1f1b710e578fa48d7a99eb467ebe79
shafakyildiz <shafakyildiz@gmail.com>
2023-12-13 20:47:11 +0300

    Style item cards

--- src/components/popular/Popular.jsx
+++ src/components/popular/Popular.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "./Popular.css";
 import { product_data } from "../assets/product_data.js";
-import Item from "../Item/Item";
+import Item from "../item/Item.jsx";
+import "./Popular.css";
 
 const Popular = () => {
   console.log(product_data);
