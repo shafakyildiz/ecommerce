d59ccc54eae1441e7842a0a9487869fbd880d0ee
shafakyildiz <shafakyildiz@gmail.com>
2023-12-14 11:23:52 +0300

    Fix popular component

--- src/components/newcollections/NewCollections.jsx
+++ src/components/newcollections/NewCollections.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Item from "../Item/Item";
+import Item from "../item/Item";
 import { new_collections } from "../assets/new_collections.js";
 import "./NewCollections.css";
 import { Link } from "react-router-dom";
