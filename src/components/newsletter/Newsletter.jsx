b078ab7316b6904423a6d4410793450e3843dd1b
shafakyildiz <shafakyildiz@gmail.com>
2023-12-14 16:24:16 +0300

    Change styling in newsletter

--- src/components/newsletter/Newsletter.jsx
+++ src/components/newsletter/Newsletter.jsx
@@ -4,7 +4,7 @@ import "./Newsletter.css";
 const Newsletter = () => {
   return (
     <div className="newsletter">
-      <h1>Get Exclusive Offers on your email...</h1>
+      <h1>Get exclusive offers by email!</h1>
       <p>Subscribe to our newsletter and stay updated.</p>
       <div className="input">
         <input type="email" placeholder="Your email address" />
