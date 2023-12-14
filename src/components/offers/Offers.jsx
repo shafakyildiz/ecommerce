9547043d093ff19cf2056d98e3e100ed9b3b49bb
shafakyildiz <shafakyildiz@gmail.com>
2023-12-14 16:22:08 +0300

    Add men route to offers check now button

--- src/components/offers/Offers.jsx
+++ src/components/offers/Offers.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import exclusive_image from "../assets/exclusive_image.png";
 import "./Offers.css";
+import { Link } from "react-router-dom";
 
 const Offers = () => {
   return (
@@ -9,7 +10,9 @@ const Offers = () => {
         <h1>Exclusive</h1>
         <h1>Offers For You</h1>
         <p>ONLY ON BESTSELLER PRODUCTS</p>
-        <button>Check Now</button>
+        <Link to="/men">
+          <button>Check Now</button>
+        </Link>
       </div>
       <div className="offers-right">
         <img src={exclusive_image} alt="" className="exclusive_image" />
